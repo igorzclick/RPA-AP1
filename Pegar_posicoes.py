8ddabb9a4c50a2b285a8e7a368d4e7897f72c92f
Igor Rozendo <ii.igoralmeida@gmail.com>
2025-04-06 18:29:23 -0300

    fix: adding comment, and changing xlsx file

--- Pegar_posicoes.py
+++ Pegar_posicoes.py
@@ -1,6 +1,11 @@
 import time
 import pyautogui
 
+#Oi professor, tudo bem?
+#este arquivo, esta aqui apenas pois esta nos requisitos do projeto,
+#porem não estamos ultilizando as posições na tela, pois elas mudam de resolução pra resolução,
+#algo que roda na minha maquina, poderia não rodar na sua, então mantivemos aqui, mas não estamos usando.
+
 def get_mouse_position():
    
     time.sleep(5)
